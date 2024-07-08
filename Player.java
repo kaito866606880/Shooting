4515fe95fdccb62226eb8f78d621cafcca32078c
kaito <zeal23310083@fsgcl.jp>
2024-07-08 09:47:38 +0900

    10-6

--- Player.java
+++ Player.java
@@ -29,6 +29,9 @@ public class Player extends Character implements KeyListener {
 		}
 		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
 			GameWorld.playerBullets.add(new PlayerBullet(x, y, 0, -10));
+
+			System.out.println("弾の数=" + GameWorld.playerBullets.size());
+
 		}
 	}
 
