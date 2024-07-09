ec216a64eb4512054edd74f5f3982bfa1c1ca730
kaito <zeal23310083@fsgcl.jp>
2024-07-09 11:51:03 +0900

    12-1練習問題

--- DropEnemy.java
+++ DropEnemy.java
@@ -1,4 +1,18 @@
 
-public class DropEnemy {
+public class DropEnemy extends Enemy {
+	public DropEnemy(double x, double y, double vx, double vy) {
+		super(x, y, vx, vy);
+		life = 1;
+	}
 
+	public void move() {
+		super.move();
+		vy = vy + 0.1;
+	}
+	
+	public void draw(MyFrame f) {
+		f.setColor(0, 250, 0);
+		f.fillRect(x, y, 30, 10);
+		f.fillRect(x + 10, y + 10, 10, 20);
+	}
 }
