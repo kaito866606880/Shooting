7e669de338e8e24df402bd8fcc342914af6ffaad
kaito <zeal23310083@fsgcl.jp>
2024-07-09 11:15:13 +0900

    12-1

--- GameFrame.java
+++ GameFrame.java
@@ -1,15 +1,11 @@
-
 import java.util.Vector;
 public class GameFrame extends MyFrame {
 	public void run() {
 		GameWorld.player = new Player(100, 300, 0, 0);
 		GameWorld.playerBullets = new Vector<PlayerBullet>();
-
 		addKeyListener(GameWorld.player);
-
 		GameWorld.enemies = new Vector<Enemy>();
 		GameWorld.enemies.add(new EnemyBase(100, 50, 1, 0));
-
 		while (true) {
 			clear();
 			GameWorld.player.draw(this);
@@ -17,11 +13,12 @@ public class GameFrame extends MyFrame {
 
 			movePlayerBullets();
 			moveEnemies();
+			checkPlayerAndEnemies();
 
-
-			sleep(0.033);
+			sleep(0.03);
 		}
 	}
+
 	public void movePlayerBullets() {
 		int i = 0;
 		while (i < GameWorld.playerBullets.size()) {
@@ -41,6 +38,19 @@ public class GameFrame extends MyFrame {
 			Enemy e = GameWorld.enemies.get(i);
 			e.draw(this);
 			e.move();
+		}
+	}
+
+	public void checkPlayerAndEnemies() {
+		for (int i = 0; i < GameWorld.enemies.size(); i++) {
+
+			Enemy e = GameWorld.enemies.get(i);
+			if (Math.abs(e.x - GameWorld.player.x) <= 30 &&
+					Math.abs(e.y - GameWorld.player.y) <= 30) {
+
+				System.out.println("やられた!");
+				GameWorld.player.y = -1000;
+			}
 		}
 	}
 }
