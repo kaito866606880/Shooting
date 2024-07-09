a7cf3828869c0ba96b465d379d2cf6aa8b4cc6ba
kaito <zeal23310083@fsgcl.jp>
2024-07-09 11:17:22 +0900

    12-2

--- GameFrame.java
+++ GameFrame.java
@@ -10,15 +10,14 @@ public class GameFrame extends MyFrame {
 			clear();
 			GameWorld.player.draw(this);
 			GameWorld.player.move();
-
 			movePlayerBullets();
 			moveEnemies();
 			checkPlayerAndEnemies();
+			checkPlayerBulletsAndEnemies();
 
 			sleep(0.03);
 		}
 	}
-
 	public void movePlayerBullets() {
 		int i = 0;
 		while (i < GameWorld.playerBullets.size()) {
@@ -32,7 +31,6 @@ public class GameFrame extends MyFrame {
 			}
 		}
 	}
-
 	public void moveEnemies() {
 		for (int i = 0; i < GameWorld.enemies.size(); i++) {
 			Enemy e = GameWorld.enemies.get(i);
@@ -40,16 +38,43 @@ public class GameFrame extends MyFrame {
 			e.move();
 		}
 	}
-
 	public void checkPlayerAndEnemies() {
 		for (int i = 0; i < GameWorld.enemies.size(); i++) {
-
 			Enemy e = GameWorld.enemies.get(i);
 			if (Math.abs(e.x - GameWorld.player.x) <= 30 &&
 					Math.abs(e.y - GameWorld.player.y) <= 30) {
-
 				System.out.println("やられた!");
 				GameWorld.player.y = -1000;
+			}
+		}
+	}
+
+	public void checkPlayerBulletsAndEnemies() {
+		int i = 0;
+		while (i < GameWorld.playerBullets.size()) {
+
+			// プレイヤー弾一つ一つについて、変数bに入れて繰り返し実行する
+			PlayerBullet b = GameWorld.playerBullets.get(i);
+			int j = 0;
+			int hits = 0;
+			while (j < GameWorld.enemies.size()) {
+
+				// 敵一つ一つについて、変数eに入れて繰り返し実行する
+				Enemy e = GameWorld.enemies.get(j);
+
+				// 敵e とプレイヤー弾bが衝突していたら「あたり」を表示
+				if (Math.abs(e.x - b.x) <= 30 && Math.abs(e.y - b.y) <= 30) {
+					System.out.println("あたり");
+					hits++;
+					GameWorld.enemies.remove(j);
+				} else {
+					j++;
+				}
+			}
+			if (hits > 0) {
+				GameWorld.playerBullets.remove(i);
+			} else {
+				i++;
 			}
 		}
 	}
