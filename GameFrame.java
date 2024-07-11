768cf452328f57e2338ed2cf580a229c484ebab6
kaito <zeal23310083@fsgcl.jp>
2024-07-11 15:39:21 +0900

    13-1

--- GameFrame.java
+++ GameFrame.java
@@ -1,81 +1,131 @@
 import java.util.Vector;
-public class GameFrame extends MyFrame {
-	public void run() {
-		GameWorld.player = new Player(100, 300, 0, 0);
-		GameWorld.playerBullets = new Vector<PlayerBullet>();
+public class GameFrame extends MyFrame
+{
+	public void run()
+	{
+		GameWorld.player=new Player(100,300,0,0);
 		addKeyListener(GameWorld.player);
-		GameWorld.enemies = new Vector<Enemy>();
-		GameWorld.enemies.add(new EnemyBase(100, 50, 1, 0));
-		while (true) {
+		GameWorld.playerBullets=new Vector<PlayerBullet>();
+		GameWorld.enemies=new Vector<Enemy>();
+		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
+		while(true) 
+		{
 			clear();
 			GameWorld.player.draw(this);
 			GameWorld.player.move();
 			movePlayerBullets();
-			moveEnemies();
-			checkPlayerAndEnemies();
+			moveEnemmies();
+			PlayerAndEnemies();
 			checkPlayerBulletsAndEnemies();
+			if(GameWorld.enemies.size()==0) 
+			{
+				setColor(0,0,0);
+				drawString("クリア!",100,200,40);
+			}
 			sleep(0.03);
 		}
+
 	}
-	public void movePlayerBullets() {
-		int i = 0;
-		while (i < GameWorld.playerBullets.size()) {
-			PlayerBullet b = GameWorld.playerBullets.get(i);
+	public void movePlayerBullets() 
+	{
+		int i=0;
+		while(i<GameWorld.playerBullets.size()) 
+		{
+			PlayerBullet b=GameWorld.playerBullets.get(i);
 			b.draw(this);
 			b.move();
-			if (b.y < 0) {
+			if(b.y<0) 
+			{
 				GameWorld.playerBullets.remove(i);
-			} else {
+			}
+			else 
+			{
 				i++;
 			}
+			    
 		}
 	}
-	public void moveEnemies() {
-		for (int i = 0; i < GameWorld.enemies.size(); i++) {
-			Enemy e = GameWorld.enemies.get(i);
+	
+	public void moveEnemmies() 
+	{
+		for(int i=0;i<GameWorld.enemies.size();i++) 
+		{
+			Enemy e=GameWorld.enemies.get(i);
 			e.draw(this);
 			e.move();
 		}
-	}
-	public void checkPlayerAndEnemies() {
-		for (int i = 0; i < GameWorld.enemies.size(); i++) {
-			Enemy e = GameWorld.enemies.get(i);
-			if (checkHit(GameWorld.player, e)) {
-				System.out.println("やられた!");
-				GameWorld.player.y = -1000;
+		int i=0;
+		while(i<GameWorld.enemies.size()) 
+		{
+			Enemy e=GameWorld.enemies.get(i);
+			if(e.y>400) 
+			{
+				GameWorld.enemies.remove(i);
 			}
-		}
-	}
-	public void checkPlayerBulletsAndEnemies() {
-		int i = 0;
-		while (i < GameWorld.playerBullets.size()) {
-			// プレイヤー弾一つ一つについて、変数bに入れて繰り返し実行する
-			PlayerBullet b = GameWorld.playerBullets.get(i);
-			int j = 0;
-			int hits = 0;
-			while (j < GameWorld.enemies.size()) {
-				// 敵一つ一つについて、変数eに入れて繰り返し実行する
-				Enemy e = GameWorld.enemies.get(j);
-				// 敵e とプレイヤー弾bが衝突していたら「あたり」を表示
-				if (checkHit(e, b)) {
-					System.out.println("あたり");
-					hits++;
-					e.life--;
-				}
-				if (e.life <= 0) {
-					GameWorld.enemies.remove(j);
-				} else {
-					j++;
-				}
-			}
-			if (hits > 0) {
-				GameWorld.playerBullets.remove(i);
-			} else {
+			else 
+			{
 				i++;
 			}
 		}
 	}
-	public boolean checkHit(Character a, Character b) {
-		return Math.abs(a.x - b.x) <= 20 && Math.abs(a.y - b.y) <= 20;
+	public void PlayerAndEnemies() 
+	{
+		
+			for(int i=0;i<GameWorld.enemies.size();i++)
+			{
+				Enemy e=GameWorld.enemies.get(i);
+				if(CheckHit(GameWorld.player,e)) 
+				{
+					System.out.println("やられた");
+					GameWorld.player.y=-1000;
+				}
+			}
+		
+	}
+	
+	public void checkPlayerBulletsAndEnemies()
+	{
+		int i=0;
+		while(i<GameWorld.playerBullets.size()) 
+		{
+			PlayerBullet b=GameWorld.playerBullets.get(i);
+			int j=0;
+			int hits=0;
+				while(j<GameWorld.enemies.size()) 
+				{
+					Enemy e=GameWorld.enemies.get(j);
+			
+					if(CheckHit(e,b)) 
+					{
+						System.out.println("あたり");
+						hits++;
+						e.life--;
+					}
+					if(e.life<=0) 
+					{
+						GameWorld.enemies.remove(j);
+					}
+					else 
+					{
+						j++;
+					}
+			    }
+				if(hits>0) 
+				{
+					GameWorld.playerBullets.remove(i);
+					
+				}
+				else 
+				{
+					i++;
+				}
+			    
+		 }
+		
+	}
+	public boolean CheckHit(Character a,Character b)
+	{
+		return Math.abs(a.x-b.x)<=18 && Math.abs(a.y-b.y)<=10;
+		
 	}
 }
